c82e684f36c024d6a57f8ed7901fb43a01f8c865
bricefrisco <bricemfrisco@gmail.com>
2021-05-09 11:57:09 -0500

    Soft-code backend endpoint URI

--- frontend/src/components/UpdateButton.tsx
+++ frontend/src/components/UpdateButton.tsx
@@ -31,7 +31,7 @@ const UpdateButton = ({ summonerName }: Props) => {
     setLoading(true);
     setSuccess(false);
 
-    fetch(`http://localhost:8080/riot/${region}/summoners/${summonerName}`, {headers: {Authorization: betaKey}})
+    fetch(`${process.env.REACT_APP_BACKEND_URI}/riot/${region}/summoners/${summonerName}`, {headers: {Authorization: betaKey}})
       .then(parseResponse)
       .then((summoner: any) => {
         dispatch(updateSummoner(summoner));
